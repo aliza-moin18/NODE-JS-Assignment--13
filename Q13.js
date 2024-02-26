2ef151c5a1fcb1c3581e6300062d35e6d347b2b9
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-27 00:23:06 +0500

    Add files via upload

---
+++ Q13.js
@@ -0,0 +1,8 @@
+// 13. Your Oywn Array: Think of your e mode of tramnsporatation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these DataTransferItemList, such as "I would like to own a Honda motorcycle."
+var vehichles = [
+    "Honda city",
+    "Hilux",
+    "Audi",
+    "Farari"
+];
+vehichles.map(function (items) { return console.log("I would like to own a ".concat(items)); });
---
+++ Q13.ts
@@ -0,0 +1,10 @@
+// 13. Your Oywn Array: Think of your e mode of tramnsporatation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these DataTransferItemList, such as "I would like to own a Honda motorcycle."
+
+let  vehichles: string[] = [
+    "Honda city",
+     "Hilux",
+     "Audi",
+     "Farari"
+];
+
+vehichles.map((items) => console.log(`I would like to own a ${items}`));
